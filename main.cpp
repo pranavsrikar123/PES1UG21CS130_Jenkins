1372f9d05b7e887089a4419ea7a7a830fef42819
pranavsrikar123 <113706355+pranavsrikar123@users.noreply.github.com>
2024-03-08 16:12:17 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, Pranav\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "I have successfully built and run\n"
   return 0;
 }
