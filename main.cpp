a55e18bb0dc37b0944edc5b80d2c176cf57047fc
pranavsrikar123 <113706355+pranavsrikar123@users.noreply.github.com>
2024-03-08 16:09:13 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, Pranav\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
